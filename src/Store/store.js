c08f7ef72835912eaf6a3cbe52915e6e603762b3
amoskyalo <amoskyalo927@gmail.com>
2022-12-09 14:19:43 +0300

    added persist logic to store cart data to local storage

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,8 +1,32 @@
 import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
 import CartSlice from "../Components/Cart/CartSlice";
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const reducers = combineReducers({ cart: CartSlice });
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 export default configureStore({
-    reducer: {
-        cart: CartSlice,
-    }
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
--- src/index.js
+++ src/index.js
@@ -6,13 +6,19 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./Store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+const persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+         <PersistGate loading={null} persistor={persistor}>
+         <App />
+         </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
